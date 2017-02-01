b24d27397b8aa31f4546925157bbe57bb6c8e1eb
null_endian <null_endian@protonmail.com>
2017-02-01 11:30:01 -0800

    added ui.c and ui_print_help()

---
+++ src/ui.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void ui_print_help()
+{
+  printf("USAGE: ");
+  printf("scanr [options] <hostname|ip4|ipv6>\n");
+  printf("-----------------------------------\n");
+  printf("OPTIONS:\n")
+  printf("-t\tPort connection timeout (default: 30 sec)\n");
+  printf("-i\tPort analysis\n");
+  printf("-p\tPort number\n");
+  printf("-l\tPort range low (default: 1)\n");
+  printf("-h\tPort range high (default: 65535)\n");
+  printf("-t\tNumer of threads (default: 2)\n");
+  printf("-v\tPrint software version\n");
+  printf("-h\t Print help\n");
+
+
+}
