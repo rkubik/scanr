148e320f46b2834d8e4f0ed0b20f311302b7c668
null_endian <null_endian@protonmail.com>
2017-02-08 01:43:21 -0800

    added getopt functionality

--- src/scanr.c
+++ src/scanr.c
@@ -1,57 +1,127 @@
 #include "scanr/scan.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
+#include <config.h>
+#include <getopt.h>
+#include<ctype.h>
+#define INVALID_PORT "Invalid port number, please supply a number 1-65535.\n"
+void ui_print_help();
+static void handle_input(int argc, char **argv);
+static bool validate_port_number(int number);
 int main(int argc, char **argv)
 {
-    int i, sock, start, end;
-    char hostname[100];
+    handle_input(argc,argv);
+    return(EXIT_SUCCESS);
+}
 
-    printf("Enter hostname or IP:");
-    if(fgets(hostname,99,stdin) == NULL){
-      printf("Improper input given. Exiting...\r\n");
-      exit(1);
-    }
+/**
+ * @brief Fires appropriate module based upon user input options
+ *
+ * @param[in] argc - number of arguments passed into program
+ * @param[in] argv - Arguments passed into program
+ * @param[out] TODO(endian) - do we need an out?
+ */
+static void handle_input(int argc, char **argv)
+{
+  int port_number = 0;
+  int t_flag = 0;
+  int i_flag = 0;
+  int p_flag = 0;
+  int l_flag = 0;
+  int u_flag = 0;
+  int o_flag = 0;
+  int v_flag = 0;
+  int h_flag = 0;
 
-    printf("\n");
+  char *cvalue = NULL;
+  int index;
+  int c;
 
-    printf("Enter start port number: ");
-    if (scanf("%d", &start) != 1) {
-        printf("Improper input given. Exiting...\n");
-        exit(1);
-    }
+  int opterr = 0;
 
-    printf("\n");
-
-    printf("Enter end port number: ");
-    if (scanf("%d", &end) != 1) {
-        printf("Improper input given. Exiting...\n");
-        exit(1);
-    }
-
-    for(i = start;  i <= end; i++) {
-        scanr_port_result_t result = {0};
-
-        printf("Scanning port %i: ", i);
-
-        if (scanr_scan_port(hostname, i, &result) != 0) {
-            printf("ERROR\n");
-        } else {
-            switch (result.state) {
-                case SCANR_PORT_CLOSED:
-                    printf("CLOSED\n");
-                    break;
-                case SCANR_PORT_OPEN:
-                    printf("OPEN\n");
-                    break;
-                default:
-                    printf("UNKNOWN\n");
-                    break;
-            }
+  while ((c = getopt (argc, argv, "t:ip:l:u:o:vh")) != -1)
+  {
+    switch (c)
+      {
+      case 't':
+        t_flag = 1;
+        int threads_count = atoi(optarg);
+        /*TODO: Add validation*/
+        break;
+      case 'i':
+        i_flag = 1;
+        break;
+      case 'p':
+        p_flag = 1;
+        /*get the argument supplied to p*/
+        port_number = atoi(optarg);
+        if(!validate_port_number(port_number)){
+          printf(INVALID_PORT);
+          return;
         }
-    }
+        break;
+      case 'l':
+        l_flag = 1;
+        int port_number_lower = atoi(optarg);
+        if(!validate_port_number(port_number_lower)){
+          printf(INVALID_PORT);
+          return;
+        }
+        break;
+      case 'u':
+        u_flag = 1;
+        int port_number_upper = atoi(optarg);
+        if(!validate_port_number(port_number_upper)){
+          printf(INVALID_PORT);
+          return;
+        }
+        break;
+      case 'o':
+        o_flag = 1;
+        int conn_timeout_seconds = atoi(optarg);
+        /*TODO: Add validation*/
+        break;
+      case 'v':
+        printf("scanr %s\n", SCANR_VERSION);
+        printf("License MIT: The MIT License <https://opensource.org/licenses/MIT>\n");
+        exit(1);
+        break;
+      case '?':
+        if (optopt == 'o' || optopt == 'p'
+         || optopt == 'l' || optopt == 'u'
+         || optopt == 't'){
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        }
+        else if (isprint (optopt)){
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        }
+        else{
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        }
 
-    return 0;
+      case 'h':
+      default:
+          ui_print_help();
+          exit(1);
+          break;
+      }
+    }
+    if (argv[optind] == NULL) {
+    printf("Missing target\n");
+    exit(1);
+  }
+  if(p_flag == 1){
+    scanr_port_result_t result;
+    scanr_scan_port(argv[optind],port_number,&result);
+  }
+      exit(1);
+}
+
+static bool validate_port_number(int number)
+{
+  return (number > 65535 || number < 1) ? true : false;
 }
