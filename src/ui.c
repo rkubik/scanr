c813118c9239cc516aace137e4d7d70b294f6e81
null_endian <null_endian@protonmail.com>
2017-02-08 01:40:43 -0800

    Set up getopt

--- src/ui.c
+++ src/ui.c
@@ -7,13 +7,13 @@ void ui_print_help()
   printf("USAGE: ");
   printf("scanr [options] <hostname|ip4|ipv6>\n");
   printf("-----------------------------------\n");
-  printf("OPTIONS:\n")
-  printf("-t\tPort connection timeout (default: 30 sec)\n");
+  printf("OPTIONS:\n");
+  printf("-o\tPort connection timeout (default: 30 sec)\n");
   printf("-i\tPort analysis\n");
   printf("-p\tPort number\n");
   printf("-l\tPort range low (default: 1)\n");
-  printf("-h\tPort range high (default: 65535)\n");
-  printf("-t\tNumer of threads (default: 2)\n");
+  printf("-u\tPort range high (default: 65535)\n");
+  printf("-t\tNumber of threads (default: 2)\n");
   printf("-v\tPrint software version\n");
   printf("-h\t Print help\n");
 
